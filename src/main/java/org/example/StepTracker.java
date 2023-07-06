7c330690059cc1877324d561bce1754883c03bd9
Vladislav ilin <nilindalv@yandex.ru>
2023-07-06 22:59:44 +0300

    Реализация класса StepTracker

--- src/main/java/org/example/StepTracker.java
+++ src/main/java/org/example/StepTracker.java
@@ -1,4 +1,59 @@
 package org.example;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public class StepTracker {
+
+    private int numberOfSteps = 10000;
+    HashMap<String, MonthData> monthData = new HashMap<>();
+    String[] months = {"Январь", "Февраль", "Март", "Апрель",
+            "Май", "Июнь", "Июль", "Август", "Сентябрь",
+            "Октябрь", "Ноябрь", "Декабрь"};
+
+    protected int dayOfMonth = 30;
+
+    public StepTracker() {
+        for (int i = 0; i < 12; i++) {
+            monthData.put(months[i], new MonthData());
+        }
+    }
+
+    public void saveSteps(String month, int dayNumber, int numberSteps) {
+        monthData.get(month).dayOfMonths.put(dayNumber, numberSteps);
+        System.out.println("Данные успешно сохранены");
+    }
+
+
+
+
+
+    public void setNumberOfSteps(int numberOfSteps) {
+        if (numberOfSteps < 0) {
+            System.out.println("Количество шагов не может быть отрицательным.");
+            return;
+        }
+        this.numberOfSteps = numberOfSteps;
+    }
+
+    public int getDayOfMonth() {
+        return dayOfMonth;
+    }
+
+     class MonthData {
+        Map<Integer, Integer> dayOfMonths = new HashMap<>();
+
+        public MonthData() {
+            for (int i = 0; i <= 30; i++) {
+                dayOfMonths.put(i, 0);
+
+            }
+        }
+
+
+    }
 }
